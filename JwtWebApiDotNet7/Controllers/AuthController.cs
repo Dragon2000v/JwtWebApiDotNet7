08bc7286e2d31ca0f407ed6c9dfe734f8bf3d88d
Dragon2000v <shmatko064@gmail.com>
2023-10-16 17:25:47 +0300

    adding classes User and User, adding a controller and functions, registration with bicrypt and login encryption

---
+++ JwtWebApiDotNet7/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using JwtWebApiDotNet7.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+namespace JwtWebApiDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password.");
+            }           
+
+            return Ok(user);
+        }
+    }
+}
